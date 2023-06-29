09bd1aff2e32bd71727e03c0a04638c7184063f9
Asahel Enock Kigen <kirwaasahel@gmail.com>
2023-06-29 14:54:27 +0300

    [UPDATE]: code update

--- initiate_call.py
+++ initiate_call.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-username = 'musat'
-api_key = 'eb98c94d1854bd11d797e702f4eaff766ca62d751add2e68c0f50a6d423722cf'
-at_number = '+254702639254'
+username = os.environ.get('AT_USERNAME')  
+api_key = os.environ.get('AT_APIKEY')
+at_number = os.environ.get('AT_NUMBER')
 
 africastalking.initialize(username, api_key)
 
@@ -24,7 +24,3 @@ class VOICE:
             print (result)
         except Exception as e:
             print ("Encountered an error while making the call:%s" %str(e))
-
-call_to = "+254742079321"
-make_call = VOICE(call_to)
-make_call.call()
